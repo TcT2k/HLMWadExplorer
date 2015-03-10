0ac7b35d94a59016b05f608c63c16749ca931d27
Tobias Taschner <github@tc84.de>
2015-03-10 22:28:12 +0100

    Initial commit
    Implemented basic file extraction and picture preview

---
+++ WADArchive.h
@@ -0,0 +1,70 @@
+/*
+** HLMWadExplorer
+**
+** Coypright (C) 2015 Tobias Taschner <github@tc84.de>
+**
+** Licensed under GPL v3 or later
+*/
+
+#include <wx/wx.h>
+
+#include <wx/vector.h>
+
+class WADArchiveEntry
+{
+public:
+	WADArchiveEntry(const wxString& fileName, wxInt64 size, wxInt64 offset):
+		m_fileName(fileName),
+		m_size(size),
+		m_offset(offset)
+	{
+
+	}
+
+	const wxString& GetFileName() const
+	{
+		return m_fileName;
+	}
+
+	wxInt64 GetSize() const
+	{
+		return m_size;
+	}
+
+	wxInt64 GetOffset() const
+	{
+		return m_offset;
+	}
+
+private:
+	wxString m_fileName;
+	wxInt64 m_size;
+	wxInt64 m_offset;
+};
+
+class WADArchive
+{
+public:
+	WADArchive(const wxString& fileName);
+
+	size_t GetEntryCount()
+	{
+		return m_entries.size();
+	}
+
+	const WADArchiveEntry& GetEntry(size_t index)
+	{
+		return m_entries[index];
+	}
+
+	bool Extract(const WADArchiveEntry& entry, const wxString& targetFileName);
+
+	bool Extract(const WADArchiveEntry& entry, wxOutputStream& oStr);
+
+private:
+	wxString m_fileName;
+	wxVector<WADArchiveEntry> m_entries;
+	wxFileOffset m_dataOffset;
+
+	void ParseFile();
+};
