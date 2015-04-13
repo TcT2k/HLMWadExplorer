a1a6237a712c9b61500c6face5a520a3c0bc4d32
Tobias Taschner <github@tc84.de>
2015-04-13 10:29:56 +0200

    Fixed patch column on OSX

--- ExploreFrame.cpp
+++ ExploreFrame.cpp
@@ -121,6 +121,7 @@ ExploreFrame::ExploreFrame( wxWindow* parent ):
 		wxDLG_UNIT(this, wxSize(16, -1)).GetWidth(), wxALIGN_CENTER, wxCOL_HIDDEN);
 	m_fileListCtrl->AppendTextColumn(_("Name"), 1, wxDATAVIEW_CELL_INERT, 250);
 	m_fileListCtrl->AppendTextColumn(_("Size"), 2, wxDATAVIEW_CELL_INERT, -1, wxALIGN_RIGHT);
+	m_fileListToggleColumn->SetHidden(true);
 
 	m_fileHistory.UseMenu(m_fileMenu);
 	m_fileHistory.Load(*wxConfigBase::Get());
